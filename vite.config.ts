bd15d789d581d1cc0addcb40e961056134441e1e
Pedro <pedro.pelayo003@gmail.com>
2024-05-19 23:07:32 +0200

    Modificado la configuracion para githuib pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,30 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve : {
-    alias : {
-      '@' : path.resolve(__dirname, './src'),
-      '@res': path.resolve(__dirname, './src/resources'),
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve : {
+//     alias : {
+//       '@' : path.resolve(__dirname, './src'),
+//       '@res': path.resolve(__dirname, './src/resources'),
+//     }
+//   }
+// })
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@res': path.resolve(__dirname, './src/resources'),
+      }
     }
   }
+
+  if (command !== 'serve') {
+    config.base = '/react-diw-tarea06/'
+  }
+
+  return config
 })
